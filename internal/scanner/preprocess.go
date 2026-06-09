46c65b169f30aa8d301af83345af42615d5d1462
kadenh <kadenlukeharris6@gmail.com>
2026-06-09 14:36:49 -0700

    add image preprocessor

---
+++ internal/scanner/preprocess.go
@@ -0,0 +1,26 @@
+package scanner
+
+import (
+	"image"
+
+	"gocv.io/x/gocv"
+)
+
+func preprocess(src gocv.Mat, dst *gocv.Mat) {
+	gray := gocv.NewMat()
+	defer gray.Close()
+
+	blur := gocv.NewMat()
+	defer blur.Close()
+
+	thresh := gocv.NewMat()
+	defer thresh.Close()
+
+	kernel := gocv.GetStructuringElement(gocv.MorphRect, image.Pt(3, 3))
+	defer kernel.Close()
+
+	gocv.CvtColor(src, &gray, gocv.ColorBGRToGray)
+	gocv.GaussianBlur(gray, &blur, image.Pt(5, 5), 0, 0, gocv.BorderDefault)
+	gocv.AdaptiveThreshold(blur, &thresh, 255, gocv.AdaptiveThresholdMean, gocv.ThresholdBinaryInv, 11, 2)
+	gocv.MorphologyEx(thresh, dst, gocv.MorphClose, kernel)
+}
