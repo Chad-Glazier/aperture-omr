064280414a5672086cab32675dd1459de687dfd3
kadenh <kadenlukeharris6@gmail.com>
2026-06-09 14:14:51 -0700

    add path resolver & optional display flag

---
+++ internal/scanner/display.go
@@ -0,0 +1,23 @@
+package scanner
+
+import (
+	"fmt"
+
+	"gocv.io/x/gocv"
+)
+
+func Display(img gocv.Mat, title string) {
+	window := gocv.NewWindow(title)
+	defer window.Close()
+
+	window.ResizeWindow(1000, 1414)
+	window.IMShow(img)
+
+	fmt.Printf("Press [Esc] to continue...")
+
+	for {
+		if window.WaitKey(0)&0xFF == 27 {
+			break
+		}
+	}
+}
---
+++ internal/scanner/path.go
@@ -0,0 +1,39 @@
+package scanner
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func Resolve(path string) (string, error) {
+	if path == "" {
+		return "", fmt.Errorf("path cannot be empty")
+	}
+
+	if strings.HasPrefix(path, "~") {
+		home, err := os.UserHomeDir()
+
+		if err != nil {
+			return "", fmt.Errorf("unable to resolve home directory: %v", err)
+		}
+
+		if path == "~" {
+			path = home
+		} else if strings.HasPrefix(path, "~/") {
+			path = filepath.Join(home, path[2:])
+		}
+	}
+
+	if filepath.IsAbs(path) {
+		return filepath.Clean(path), nil
+	}
+
+	abs, err := filepath.Abs(path)
+	if err != nil {
+		return "", fmt.Errorf("unable to resolve absolute path: %v", err)
+	}
+
+	return filepath.Clean(abs), nil
+}
