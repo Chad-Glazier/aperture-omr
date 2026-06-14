83848f2834f50d3cb2bb1caf2c66ae234627a62e
kadenh <kadenlukeharris6@gmail.com>
2026-06-14 08:47:31 -0700

    removed edge detector in binarize

--- internal/scanner/scanner.go
+++ internal/scanner/scanner.go
@@ -91,19 +91,13 @@ func binarize(src gocv.Mat, dst *gocv.Mat) error {
 	kernel := gocv.GetStructuringElement(gocv.MorphRect, image.Pt(3, 3))
 	defer kernel.Close()
 
-	closing := gocv.NewMat()
-	defer closing.Close()
-
 	gocv.CvtColor(src, &gray, gocv.ColorBGRToGray)
 	gocv.GaussianBlur(gray, &blur, image.Pt(5, 5), 0, 0, gocv.BorderDefault)
 
 	// Replaces adaptive thresholding because binary threshold better preserves
 	// pencil marks within bubbles
 	gocv.Threshold(blur, &thresh, 0, 255, gocv.ThresholdBinaryInv|gocv.ThresholdOtsu)
-	gocv.MorphologyEx(thresh, &closing, gocv.MorphClose, kernel)
-
-	// Added edge detection which improves line detection during deskewing
-	gocv.Canny(closing, dst, 50, 150)
+	gocv.MorphologyEx(thresh, dst, gocv.MorphClose, kernel)
 
 	return nil
 }
