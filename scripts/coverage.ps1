40a666a41a3e9350d9f296882f95ca2381ad4146
Chad Glazier <chadglazier@outlook.com>
2026-07-23 18:19:47 -0700

    updated scripts

---
+++ scripts/coverage.ps1
@@ -0,0 +1,10 @@
+go test `
+	./internal/fs `
+	./internal/httpserver/handler `
+	./internal/httpserver/middleware `
+	./internal/marker `
+	./internal/pdf `
+	./internal/scanner `
+    -coverprofile="coverage.out"
+go tool cover -func="coverage.out"
+rm coverage.out
